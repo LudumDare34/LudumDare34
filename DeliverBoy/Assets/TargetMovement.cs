2bcafb99c194948c3b42a07ae134a59df9ff395d
Laura <l4depende@gmail.com>
2015-12-12 15:34:25 +0000

    Objetivos que se mueven

---
+++ DeliverBoy/Assets/TargetMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetMovement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 movement = new Vector3 (1,1,0.0f);
+		rb.velocity = movement.normalized * speed;
+	}
+
+	void OnBecameInvisible(){
+		Debug.Log ("I became invisible");
+		Destroy(gameObject);
+	}
+}
---
+++ DeliverBoy/Assets/TargetSpawnPoint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetSpawnPoint : MonoBehaviour {
+
+	public enum SpawnState{
+		Creating,
+		Waiting
+	}
+
+
+	public GameObject prefab;
+	public float secondsToSpawn;
+	private float secondsPassed;
+
+	// Use this for initialization
+	void Start () {
+		Camera  mainCamera = FindObjectOfType<Camera> ();
+	}
+	
+	void Update(){
+		if (secondsPassed * Time.deltaTime > secondsToSpawn) {
+			secondsPassed = 0f;
+			Creating ();
+			return;
+		} else {
+			secondsPassed += 0.1f;
+		}
+	}
+
+	void Creating(){
+		Instantiate (prefab, transform.position, Quaternion.identity);
+	}
+
+
+}
